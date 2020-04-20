c1f3ac78211c562103cf2a0853cabd536c72d954
Destiny <destiny6@mail.usf.edu>
2020-04-20 00:31:00 -0400

    created form

--- employee/src/pages/TimeOff.js
+++ employee/src/pages/TimeOff.js
@@ -4,7 +4,44 @@ import Axios from 'axios'
 export default () => {
 
     return <>
+       <body>
+        <form method="POST" action="cp.php"> 
         <h1>Time Off Request Form</h1>
-        <p>A time off request form will display below</p>
+            <div style={{backgroundColor: "lightblue"}}>
+            <p>Name: </p>
+                <input
+                    type="text" name="name"
+                />
+
+            <br></br>
+            <br></br>
+
+            <p>Employee ID: </p>
+                <input
+                        type="text" name="id"
+                />   
+
+            <br></br>
+            <br></br>
+
+            <p>Leave Date: </p>
+                <input
+                        type="text" name="leaveDate"
+                />  
+
+            <br></br>
+            <br></br>
+
+            <p>Return Date: </p>
+                <input
+                        type="text" name="returnDate"
+                />               
+            </div>
+
+            <br></br>
+
+            <input type="submit" value="Submit" href="cp.php"></input>
+        </form>
+    </body>
     </>
 }
