031d343d921bea8c7bed22d940ab59d5b486c9b0
Javier Quiroga Almeida <quiroga218@gmail.com>
2020-04-19 23:28:03 -0500

    Change a lot

--- employee/src/pages/TimeOff.js
+++ employee/src/pages/TimeOff.js
@@ -3,16 +3,6 @@ import Axios from 'axios'
 
 export default () => {
 
-    useEffect(() => {
-        Axios.get('/ping').then((axiosResponse) => {
-            debugger
-            console.log(axiosResponse.data)
-        }).catch((AxiosError) => {
-            console.log(AxiosError)
-        })
-    },
-    [])
-
     return <>
         <h1>Time Off Request Form</h1>
         <p>A time off request form will display below</p>
